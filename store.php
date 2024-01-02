84bc479ce2a0f45a43584126d88b0521d509fcd0
ReynaldyNC <reynaldy.nur.cahyo.003@gmail.com>
2024-01-02 12:21:18 +0700

    Dokumentasi

--- store.php
+++ store.php
@@ -2,33 +2,43 @@
 
 require("config.php");
 
+// Pattern untuk mencari angka tahun
 $reg_age = "/[0-9]/";
-$reg_age_omit = "/tahun|th|thn/i";
+// Pattern untuk mencari kata tahun/thn/th. case-insensitivity
+$reg_age_omit = "/tahun|thn|th/i";
 
 $data = $_POST['data'];
 
+// Cari kata yang sesuai dengan pattern $reg_age_omit, kemudian hilangkan
 $data = preg_filter($reg_age_omit, '', $data);
 
-$str = preg_split($reg_age, $data, -1, PREG_SPLIT_NO_EMPTY);
-$name = trim($str[0]);
-$city = trim($str[1]);
+// Pisahkan antara nama dan kota dengan umur sebagai pemisah 
+$str = preg_split($reg_age, $data, 0, PREG_SPLIT_NO_EMPTY);
+// Ubah nama dan kota menjadi UPPERCASE dan hilangkan spasi yang tidak berguna
+$name = trim(strtoupper($str[0]));
+$city = trim(strtoupper($str[1]));
 
+// Ambil umur dengan menjadikan panjang nama dan posisi index awal kota sebagai patokan
 $age_start = strpos($data, $name) + strlen($name);
 $age_end = strpos($data, $city);
 $age = substr($data, $age_start, $age_end - $age_start);
 
-echo "Name: $name";
-echo "<br>";
-echo "Age: $age";
-echo "<br>";
-echo "City: $city";
+// echo "Name: $name";
+// echo "<br>";
+// echo "Age: $age";
+// echo "<br>";
+// echo "City: $city";
 
+// Masukkan data ke dalam database
 $sql = "INSERT INTO arkatama(name, age, city) VALUES('$name', '$age', '$city')";
 
+// Jika berhasil, maka kembali ke index.php
 if ($conn->query($sql)) {
     echo "Success!";
     header("Location: /index.php");
 }
+
+// Tutup
 $conn->close();
 
 ?>
